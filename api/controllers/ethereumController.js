d83cf4a1beee55d92187fa960c88334279adef2b
venkatsaipalla <venkatasaipalla2019@email.com>
2024-03-28 22:24:52 +0530

    added etherem wallet check api

--- api/controllers/ethereumController.js
+++ api/controllers/ethereumController.js
@@ -1,16 +1,19 @@
 // controllers/ethereumController.js
-import Web3 from 'web3';
+import Web3 from "web3";
 
 // Initialize Web3 with Infura node
-const web3 = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_API_KEY');
+const provider = new Web3.providers.HttpProvider(
+  "https://mainnet.infura.io/v3/5a9a800083b743839b307fa3f395554d"
+);
+const web3 = new Web3(provider);
 
 export const getEthBalance = async (req, res) => {
   try {
     const balanceWei = await web3.eth.getBalance(req.params.address);
-    const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
+    const balanceEth = web3.utils.fromWei(balanceWei, "ether");
     res.json({ balance: balanceEth });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Error fetching balance' });
+    res.status(500).json({ error: "Error fetching balance" });
   }
 };
--- api/routes/ethereumRoutes.js
+++ api/routes/ethereumRoutes.js
@@ -1,10 +1,11 @@
 // ethereumRoutes.js
 import express from 'express';
 import { getEthBalance } from '../controllers/ethereumController.js';
+import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // Route to get Ethereum account balance
-router.get('/:address', getEthBalance);
+router.get('/:address',verifyToken, getEthBalance);
 
 export default router;
